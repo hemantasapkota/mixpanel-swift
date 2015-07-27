b47d897e164ee120018bae0afd686de7510ed740
Hemanta Sapkota <laex.pearl@gmail.com>
2015-07-27 15:38:16 +1000

    Initial commit

---
+++ MixPanel/MixPanel/MixPanel.h
@@ -0,0 +1,20 @@
+//
+//  MixPanel.h
+//  MixPanel
+//
+//  Created by Hemanta Sapkota on 27/07/2015.
+//  Copyright (c) 2015 Hemanta Sapkota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MixPanel.
+FOUNDATION_EXPORT double MixPanelVersionNumber;
+
+//! Project version string for MixPanel.
+FOUNDATION_EXPORT const unsigned char MixPanelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MixPanel/PublicHeader.h>
+
+
+#import <Mixpanel.h>
